825c010da7979253e288d3108ff30909462602ad
= <naseev.adhikari@outlook.com>
2022-12-16 17:10:15 +0545

    Updated and Added code for Lab 4 Order Authentication

--- src/order-api/functions/get-single-order/app.js
+++ src/order-api/functions/get-single-order/app.js
@@ -1,39 +1,43 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-const tableName = process.env.ORDER_TABLE;
-const dynamodb = require('aws-sdk/clients/dynamodb');
-const docClient = new dynamodb.DocumentClient();
+const tableName = process.env.ORDER_TABLE
+const dynamodb = require("aws-sdk/clients/dynamodb")
+const docClient = new dynamodb.DocumentClient()
 
 exports.getOrderById = async (event) => {
-    if (event.httpMethod !== 'GET') {
-        throw new Error(`getAllItems only accept GET method, you tried: ${event.httpMethod}`);
-    }
-    const orderId = event.pathParameters.orderId;    // It gets orderId from url
+  if (event.httpMethod !== "GET") {
+    throw new Error(
+      `getAllItems only accept GET method, you tried: ${event.httpMethod}`
+    )
+  }
+  const orderId = event.pathParameters.orderId // It gets orderId from url
 
-    let get_item = {
-        user_id : "static_user",   
-        id: orderId
-    }
+  let get_item = {
+    user_id:
+      event.requestContext.authorizer.claims["cognito:username"] ||
+      event.requestContext.authorizer.claims.username, // As we are using Lambda proxy from AWS ApiGateway, the cognito username will directly pass to here.
+    id: orderId,
+  }
 
-    let params = {
-        TableName : tableName,
-        Key: get_item
-    };
-    
-    let item = null;
-    // We call the Get function to get the single Item
-    try {
-        const data = await docClient.get(params).promise();
-        item = data.Item;
-    } catch (err) {
-        console.log("Failure", err.message)
-    }
+  let params = {
+    TableName: tableName,
+    Key: get_item,
+  }
 
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(item)
-    };
+  let item = null
+  // We call the Get function to get the single Item
+  try {
+    const data = await docClient.get(params).promise()
+    item = data.Item
+  } catch (err) {
+    console.log("Failure", err.message)
+  }
 
-    return response;
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(item),
+  }
+
+  return response
 }
